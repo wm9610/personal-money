d4fee52ce8c3f58fe9d16c0e159aab9bd26475bc
wmcheng <cwm96107@gmail.com>
2022-07-17 22:50:48 +0800

    Add login and register pages

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import Sidebar from './sidebar/Sidebar';
 import Header from './header/Header';
+import {useRouter} from 'next/router';
 
 export default function Layout({children}) {
-  return (
-    <div className="flex min-h-screen relative">
-      <Sidebar />
-      <main className="flex-1 flex flex-col">
-        <Header />
-        <div className="flex-1">{children}</div>
-      </main>
-    </div>
-  );
+  const router = useRouter();
+  if (
+    router.pathname.includes('/login') ||
+    router.pathname.includes('/register')
+  ) {
+    return <div className="min-h-screen bg-slate-100">{children}</div>;
+  } else {
+    return (
+      <div className="flex min-h-screen bg-slate-100">
+        <Sidebar />
+        <main className="flex-1 flex flex-col">
+          <Header />
+          <div className="flex-1">{children}</div>
+        </main>
+      </div>
+    );
+  }
 }
