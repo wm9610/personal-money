cf4027860d4c1ff019cd7138fb750444c09c37f4
wmcheng <cwm96107@gmail.com>
2022-07-23 18:32:31 +0800

    Create api for expenses

---
+++ pages/api/expenses/[id].js
@@ -0,0 +1,73 @@
+import {doc, getDoc, deleteDoc, setDoc, Timestamp} from 'firebase/firestore';
+import {db} from '../../../firebaseConfig';
+
+export default async function ExpenseHandler(req, res) {
+  const {id} = req.query;
+  console.log(id);
+  switch (req.method) {
+    case 'GET':
+      const singleResult = await fetchSingleExpense(id);
+      return res.status(200).json(singleResult);
+
+    case 'DELETE':
+      const deleteResult = await deleteExpense(id);
+      return res.status(200).json(deleteResult);
+
+    case 'PUT':
+      const {name, category, price, date} = req.body;
+      if (name || category || price || date) {
+        const newExpense = {
+          name,
+          category,
+          price,
+          date: Timestamp.fromDate(new Date(date)),
+        };
+        const refId = await editExpense(id, newExpense);
+        return res.status(200).json(refId);
+      } else {
+        return '';
+      }
+
+    default:
+      return res.status(405).json({
+        message: `Invalid method, ${req.method} not allowed`,
+      });
+  }
+}
+
+async function fetchSingleExpense(refId) {
+  try {
+    const docRef = doc(db, 'expense', refId);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      return {status: 'OK', expense: docSnap.data()};
+    } else {
+      return {status: 'NG', expense: {}};
+    }
+  } catch (e) {
+    return {status: 'NG', expense: {}};
+  }
+}
+
+async function deleteExpense(refId) {
+  try {
+    const docRef = doc(db, 'expense', refId);
+    result = await deleteDoc(docRef);
+    return {status: 'OK'};
+  } catch (e) {
+    return {status: 'NG'};
+  }
+}
+
+async function editExpense(refId, expense) {
+  try {
+    const docRef = doc(db, 'expense', refId);
+    const result = await setDoc(docRef, {
+      ...expense,
+    });
+    console.log(result);
+    return {status: 'OK'};
+  } catch (e) {
+    return {status: 'NG'};
+  }
+}
---
+++ pages/api/expenses/index.js
@@ -0,0 +1,63 @@
+import {collection, getDocs, addDoc, Timestamp} from 'firebase/firestore';
+import {db} from '../../../firebaseConfig';
+
+export default async function ExpenseHandler(req, res) {
+  switch (req.method) {
+    case 'GET':
+      const result = await fetchAllExpenses();
+      return res.status(200).json(result);
+
+    case 'POST':
+      const {name, category, price, date} = req.body;
+      if (name && category && price && date) {
+        const newExpense = {
+          name,
+          category,
+          price,
+          date: Timestamp.fromDate(new Date(date)),
+        };
+        const result = await addExpense(newExpense);
+        return res.status(200).json(result);
+      } else {
+        return res.status(405).json({status: 'NG', expenseId: ''});
+      }
+
+    default:
+      return res.status(405).json({
+        status: 'NG',
+        message: `Invalid method, ${req.method} not allowed`,
+      });
+  }
+}
+
+async function fetchAllExpenses() {
+  try {
+    const colRef = collection(db, 'expense');
+    const querySnapshot = await getDocs(colRef);
+    const expenses = [];
+    querySnapshot.forEach((item) => {
+      const {date, ...other} = item.data();
+      expenses.push({
+        id: item.id,
+        ...other,
+        date: date.toDate(),
+        month: date.toDate().getMonth(),
+      });
+    });
+    return {status: 'OK', expenses};
+  } catch (e) {
+    return {status: 'NG', expenses: []};
+  }
+}
+
+async function addExpense(expense) {
+  try {
+    const colRef = collection(db, 'expense');
+    const docRef = await addDoc(colRef, {
+      ...expense,
+    });
+    return {status: 'OK', expenseId: docRef.id};
+  } catch (e) {
+    return {status: 'NG', expenseId: ''};
+  }
+}
